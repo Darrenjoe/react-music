de1f990cda766791c0df1d207fe434e009569772
Darrenjoe <491361994@qq.com>
2019-12-18 17:38:31 +0800

    Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { renderRoutes } from "react-router-config";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { GlobalStyle } from "./style";
+import routes from "./routes/index.js";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+      {renderRoutes(routes)}
+    </HashRouter>
   );
 }
 
