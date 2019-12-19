d24f2adc32ca94808888b5258aa0e432e1cbffd3
Darrenjoe <491361994@qq.com>
2019-12-19 16:27:48 +0800

    list

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Slider from "../../components/slider";
+import RecommendList from "../../components/list";
 
 function Recommend(props) {
   const bannerList = [1, 2, 3, 4].map(item => {
@@ -9,9 +10,20 @@ function Recommend(props) {
     };
   });
 
+  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
+    return {
+      id: 1,
+      picUrl:
+        "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+      playCount: 17171122,
+      name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+    };
+  });
+
   return (
     <div>
       <Slider bannerList={bannerList}></Slider>
+      <RecommendList recommendList={recommendList}></RecommendList>
     </div>
   );
 }
